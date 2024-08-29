e886bead9d498aa487ba68c0641dfc4e92c01eb2
diananov11 <ohlemon0678@gmail.com>
2024-08-29 13:07:45 +0700

    add description

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-// console.log(weight);
-// console.log(height);
-
 const calculateBMI = (event) => {
   event.preventDefault();
+  // add dom
   let weight = document.getElementById("weight").value;
   let height = document.getElementById("height").value;
   let p = document.getElementById("result");
-  p.innerHTML = "hai";
-  console.log(weight);
-  console.log(height);
+
+  //add calculation
   let calculation = weight / (height / 100) ** 2;
   let result = calculation.toFixed(1);
-  console.log(result);
+  let description = "";
+
+  //condition
+  if (result >= 30) {
+    description = "Obesity";
+  } else if (result >= 25) {
+    description = "Overweight";
+  } else if (result >= 18.5) {
+    description = "Normal weight";
+  } else {
+    description = "underweight";
+  }
+
+  p.innerHTML = `Your BMI is ${result} which means You are ${description}`;
 };
 
 let button = document.getElementById("button");
